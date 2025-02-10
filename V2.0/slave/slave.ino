6865330ac34e9e3525ab110b5078dcf7c2ebdb50
Marco-42 <segatomrc@gmail.com>
2025-02-10 22:56:46 +0100

    Update slave.ino

--- V2.0/slave/slave.ino
+++ V2.0/slave/slave.ino
@@ -1,5 +1,5 @@
 /*
-  Programma di trasmissione per il modulo SX1278 usando la libreria RadioLib 
+  Programma di ricezione per il modulo SX1278 usando la libreria RadioLib 
   Scheda usata: TTGO LoRa32 T3_V1.6.1 433 MHz
 
   Per le impostazioni di default per il modulo visualizzare il seguente link:
@@ -26,6 +26,23 @@
 // DIO1 pin: RADIOLIB_NC --> non dato
 SX1278 radio1 = new Module(18, 26, 23, RADIOLIB_NC);
 
+// definisco alcune variabili utili per la gestione della comunicazione in modalità interrupt
+// la ricezione o la trasmissione dei pacchetti non blocca l'esecuzione del programma
+
+// check_com indica se la ricezione è terminata 
+// viene impostata su false se la ricezione deve ancora essere ultimata 
+volatile bool check_com = false;
+
+// la seguente riga di codice ottimizza l'esecuzione del programma per l'impiego di esp32
+#if defined(ESP8266) || defined(ESP32)
+  ICACHE_RAM_ATTR
+#endif
+
+// la funzione setFlag imposta come true la variabile check_com quando viene richiamata
+void setFlag(void) {
+  check_com = true;
+}
+
 void setup() {
 
   // inizializzo la porta seriale
@@ -45,11 +62,13 @@ void setup() {
   //frequency - bandwidth - spreading factor - coding rate - sync word - output power -  preamble length - gain
   int state = radio1.begin(433.0, 125.0, 10, 8, 0x12, 10, 8, 1);
 
-  // verifico se l'inizializzazione del modulo è avvenuta correttamente
+  // verifico se l'inizializzazione del modulo è avvenuta correttamente(state memorizza lo stato dell'inizializzazione)
   if (state == RADIOLIB_ERR_NONE) {
     Serial.println(F("Starting procedure COMPLETE"));
   } else {
     Serial.print(F("Starting procedure FAILED"));
+    // mando a schermo lo stato di errore dell'inizializzazione
+    Serial.println(state);
 
   // blocco il programma nel caso di fallimento nell'inizializzazione del modulo(fino a quando l'utente non forza il riavvio)
     while (true) { delay(10); }
@@ -58,63 +77,68 @@ void setup() {
   // imposto il limite di sicurezza per la corrente a 60 mA --> impostare a 0 mA per annullare il limite di sicurezza
   // verifico se l'impostazione è avvenuta correttamente
   if (radio1.setCurrentLimit(60) == RADIOLIB_ERR_INVALID_CURRENT_LIMIT) {
-    Serial.println(F("Selected current limit is invalid for this module!"));
+    Serial.println(F("SETTING FAILED: valore della corrente limite non compatibile con il modello impiegato"));
 
   //  blocco il programma nel caso di fallimento fino a quando l'utente non forza il riavvio
     while (true) { delay(10); }
   }
 
+  // imposto la funzione che verrà richiamata quando la ricezione finisce
+  // se la ricezione si conclude correttamente verrà poi eseguita la funzione setFlag
+  radio1.setPacketReceivedAction(setFlag);
+
+  // effettuo la prima ricezione
+  Serial.print(F("[SX1278] RICEZIONE: "));
+  state = radio1.startReceive();
+
+  // verifico lo stato della ricezione
+  if (state == RADIOLIB_ERR_NONE) {
+    Serial.println(F("Ricezione avviata con successo"));
+  } else {
+    Serial.print(F("Ricezione avviata fallita --> Errore: "));
+    Serial.println(state);
+    // blocco il programma nel caso di fallimento fino a quando l'utente non forza il riavvio
+    while (true) { delay(10); }
+  }
 }
 
-// contatore pacchetti trasmessi
-int conta = 0;
-
-// Eseguo una trasmissione in blocking(trasmetto un pacchetto dati alla volta ed attendo la fine della trasmissione)
+// Eseguo una ricezione(ricevo un pacchetto dati alla volta senza bloccare l'esecuzione del programma)
 void loop() {
-  Serial.print(F("[SX1278] Trasmissione: "));
 
-  // creo una stringa contenente un messaggio testuale ed il numero della trasmissione
-  String str = "Hello World! " + String(conta++);
+// leggo il pacchetto ottenuto solo se la ricezione si è conclusa
+  if(check_com) {
+    // resetto il valore di check_com
+    check_com = false;
 
-  // trasmetto il pacchetto 
-  int state = radio1.transmit(str);
+// mando i dati ricevuti in una stringa 
+  String str;
+  int state = radio1.readData(str);
 
-  // per gestire il messaggio a partire dai singoli bit: 
-  /*
-    byte byteArr[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
-    int state = radio.transmit(byteArr, 8);
-  */
-  
-  // verifico lo stato della trasmissione del pacchetto
-  // durante questa fase il programma si blocca ed aspetta l'avvenuta trasmissione
+// se c'è la necessità di leggere i singoli bit del pacchetto: 
+    /*
+      byte byteArr[8];
+      int numBytes = radio1.getPacketLength();
+      int state = radio1.readData(byteArr, numBytes);
+    */
+
+// se la ricezione è avvenuta correttamente mando a schermo il pacchetto
   if (state == RADIOLIB_ERR_NONE) {
-    // se la trasmissione è avvenuta correttamente
-    Serial.println(F("Trasmissione completata: "));
+      // se il pacchetto è stato ricevuto con successo
+      Serial.println(F("[SX1278] Ricezione avvenuta"));
 
-    // Mando a schermo le informazioni sulla velocità della trasmissione
-    Serial.print(F("[SX1278] Velocità trasmissione:\t"));
-    Serial.print(radio1.getDataRate());
-    Serial.println(F(" bps"));
+      // mando a schermo i dati ricevuti
+      Serial.print(F("[SX1278] Dati: "));
+      Serial.println(str);
 
-   // se la trasmissione è fallita mando a schermo il messaggio d'errore
-  } else if (state == RADIOLIB_ERR_PACKET_TOO_LONG) {
-    // Se il pacchetto dati è troppo lungo(supera i 256 bit)
-    Serial.println(F("Trasmissione fallita(>256bit)"));
+ }else if(state == RADIOLIB_ERR_CRC_MISMATCH){
+  // se la ricezione è fallita per la manomissione/corruzione del pacchetto
+  Serial.println(F("[SX1278] Corruzione del pacchetto - Ricezione fallita - CRC error"));
+ 
+ }else{
+  // se la ricezione è fallita per altri motivi mando a schermo il messaggio d'errore
+  Serial.print(F("[SX1278] RICEZIONE FALLITA --> Errore: "));
+  Serial.println(state);
 
-  } else if (state == RADIOLIB_ERR_TX_TIMEOUT) {
-    // Se la trasmissione non si verifica correttamente avviene il timeout della trasmissione
-    Serial.println(F("Trasmissione fallita(TIMEOUT)"));
-
-  } else {
-    // se si sono verificati altri errori durante la trasmissione
-    Serial.print(F("TRASMISSIONE FALLITA - Stato: "));
-    Serial.println(state);
-
-  }
-
-  // Tra una trasmissione e l'altra attendo 5 secondi
-  delay(5000);
-
-
-
+ }
+}
 }
